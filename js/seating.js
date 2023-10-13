05c2a504e3b0189408021e2f1c985f3ebb090bf6
Jesper Erling Nielsen <jesp74m8@stud.kea.dk>
2023-10-13 12:54:45 +0200

    added basic functionality for seating.js and thus reservation

--- js/seating.js
+++ js/seating.js
@@ -3,9 +3,7 @@ const container = document.querySelector('.container');
 const rowContainer = document.querySelector('.container .row-container')
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.querySelector('#count');
-const total = document.querySelector('#total');
-const movieSelect = document.querySelector('#movie');
-const row = document.querySelector('.row');
+const seatsAndPrice = document.querySelector('#seats_and_price');
 const seatsURL = "http://kinoxp-back.azurewebsites.net/seats/"
 const reservedSeatsURL = "http://kinoxp-back.azurewebsites.net/reservation/seats/"
 const urlParams = new URLSearchParams(window.location.search);
@@ -17,10 +15,9 @@ initData();
 function initData(){
     const arrayOfSeats = actionFetchSeats(seatsURL+showing.theater.theaterID);
     createRows(arrayOfSeats);
-    actionFetchSeats(seatsURL+showing.showingID);
+    const arrayOfReservedSeats = actionFetchSeats(reservedSeatsURL+showing.showingID);
+    setReservedSeats(arrayOfReservedSeats);
 }
-
-let ticketPrice = +movieSelect.value;
 
 //update total and count
 function updateSelectedCount() {
@@ -33,14 +30,8 @@ function updateSelectedCount() {
     const selectedSeatCount = selectedSeats.length;
     console.log(selectedSeatCount);
     count.innerText = selectedSeatCount;
-    total.innerText = selectedSeatCount * ticketPrice;
+    seatsAndPrice.innerText = selectedSeatCount * showing.price;
 }
-
-// movie select event
-movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
-    updateSelectedCount();
-});
 
 container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
@@ -68,7 +59,16 @@ function createSeats(createRow, rowNumber, arrayOfSeats) {
     for (let seatNumber = 0; seatNumber < showing.theater.numberOfSeatsPerRow; seatNumber++) {
         const createSeat = document.createElement('div');
         createSeat.classList = 'seat';
-        createRow.value = arrayOfSeats[seatNumber*rowNumber-1];
+        let seat = arrayOfSeats[seatNumber*rowNumber-1];
+        createSeat.value = seat
+        createSeat.id = `seat${seat.seatID}`;
         createRow.appendChild(createSeat);
     }
+}
+
+function setReservedSeats(arrayOfReservedSeats) {
+    arrayOfReservedSeats.forEach(seat => {
+        const reservedSeat = document.getElementById(`seat${seat.seatID}`);
+        reservedSeat.classList.add('occupied')
+    });
 }
